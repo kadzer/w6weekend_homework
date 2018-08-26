76d40b4153e91acd1cee7ff73c9b402c846e19da
Kaspars Dzerins <kadzer@gmail.com>
2018-08-26 16:18:00 +0100

    MVP complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
+//things called after DOM is loaded happen here
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
-  console.log('we have a liftoff');
 
 });
 
 const handleFormSubmit = function(event){
   event.preventDefault();
+  const drinkUl = document.createElement('ul');
+  const nameLi = document.createElement('li');
+  nameLi.textContent = `Name: ${event.target.name.value}`;
+  const distilleryLi = document.createElement('li');
+  distilleryLi.textContent = `Distillery: ${event.target.distillery.value}`;
+  const countryLi = document.createElement('li');
+  countryLi.textContent = `Country of origin: ${event.target.country.value}`;
 
+  drinkUl.appendChild(nameLi);
+  drinkUl.appendChild(distilleryLi);
+  drinkUl.appendChild(countryLi);
+
+  const listDiv = document.querySelector('#drinklist');
+  listDiv.appendChild(drinkUl);
 }
